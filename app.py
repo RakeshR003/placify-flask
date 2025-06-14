de5c1ed8ecf12aa4cb8b525f15d069bb9288eb8a
Raghavendra Rakesh <167906855+RakeshR003@users.noreply.github.com>
2025-06-14 05:59:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -926,14 +926,18 @@ def analyse():
 
         import re
         def cleanResume(txt):
-            cleanText = re.sub('http\S+\s', ' ', txt)
-            cleanText = re.sub('RT|cc', ' ', cleanText)
-            cleanText = re.sub('#\S+\s', ' ', cleanText)
-            cleanText = re.sub('@\S+', '  ', cleanText)  
-            cleanText = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', cleanText)
-            cleanText = re.sub(r'[^\x00-\x7f]', ' ', cleanText) 
-            cleanText = re.sub('\s+', ' ', cleanText)
-            return cleanText
+             cleanText = re.sub(r'http\S+\s', ' ', txt)        # Remove URLs
+    cleanText = re.sub(r'RT|cc', ' ', cleanText)      # Remove RT/cc
+    cleanText = re.sub(r'#\S+\s', ' ', cleanText)     # Remove hashtags
+    cleanText = re.sub(r'@\S+', ' ', cleanText)       # Remove mentions
+    cleanText = re.sub(
+        r'[%s]' % re.escape(r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), 
+        ' ', 
+        cleanText
+    )                                                # Remove punctuation
+    cleanText = re.sub(r'[^\x00-\x7f]', ' ', cleanText)  # Remove non-ASCII
+    cleanText = re.sub(r'\s+', ' ', cleanText)        # Normalize whitespace
+    return cleanText
 
         # Clean the input resume
         cleaned_resume = cleanResume(str(resumes))
