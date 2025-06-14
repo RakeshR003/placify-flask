ceebdb14cdaabbc93e9128b1390c68fa64c881cf
Raghavendra Rakesh <167906855+RakeshR003@users.noreply.github.com>
2025-06-14 05:53:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Exit on error
+set -e
+# Install Python dependencies with Poetry
+poetry install --no-interaction --no-ansi --no-root
 pip install --prefer-binary scipy numpy  # Force pre-compiled wheels
 sudo apt-get update && sudo apt-get install -y gfortran
 pip install -r requirements.txt
 pip install --prefer-binary numpy
 pip install --prefer-binary scipy
+pip install --upgrade poetry
