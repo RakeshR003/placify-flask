e1749273e14ab0198c0bc9d68f14b18f12c5f256
Raghavendra Rakesh <167906855+RakeshR003@users.noreply.github.com>
2025-06-14 05:34:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 pip install --prefer-binary scipy numpy  # Force pre-compiled wheels
+sudo apt-get update && sudo apt-get install -y gfortran
 pip install -r requirements.txt
+pip install --prefer-binary numpy
+pip install --prefer-binary scipy
